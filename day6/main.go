8054ef029ca935893cfd79672ba7957ac00a7c74
Austin Bisharat <austinbisharat@gmail.com>
2020-12-05 21:51:50 -0800

    Finish day 6

---
+++ day6/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	processor := &processor{}
+	processLinesInFile("day6/input.txt", processor)
+	fmt.Printf("sum ticket: %d\n", processor.GetCount())
+}
+
+type processor struct {
+	answerSet *letterSet
+	sum uint
+}
+
+func (p *processor) ProcessLine(_ int, line string) error {
+	if line != "" {
+		ls := &letterSet{}
+
+		for _, r := range line {
+			ls.Add(r)
+		}
+
+		if p.answerSet == nil {
+			p.answerSet = ls
+		} else {
+			p.answerSet.Retain(ls)
+		}
+	} else if p.answerSet != nil {
+		p.sum += p.answerSet.GetCount()
+		p.answerSet = nil
+	}
+	return nil
+}
+
+func (p *processor) GetCount() uint {
+	if p.answerSet != nil {
+		p.sum += p.answerSet.GetCount()
+	}
+	return p.sum
+}
+
+type letterSet struct {
+	set uint32
+	count uint
+}
+
+func (l *letterSet) Add(r rune) {
+	idx := int(r) - int('a')
+	if (l.set >> idx) & 1 == 0 {
+		l.count++
+		l.set |= 1 << idx
+	}
+}
+
+func (l *letterSet) GetCount() uint {
+	return l.count
+}
+
+func (l *letterSet) Retain(other *letterSet) {
+	before := l.set
+	l.set &= other.set
+
+	bitsChanged := before ^ l.set
+	for bitsChanged != 0 {
+		bitsChanged &= bitsChanged - 1
+		l.count--
+	}
+}
