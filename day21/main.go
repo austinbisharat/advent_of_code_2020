53412801edfec7c86012acc71a1249b9480553df
Austin Bisharat <austinbisharat@gmail.com>
2020-12-21 23:01:00 -0800

    Extract formatting

--- day21/main.go
+++ day21/main.go
@@ -12,12 +12,16 @@ func main() {
 	lineprocessor.ProcessLinesInFile("day21/input.txt", p)
 	fmt.Printf("Confirmed non-allergen count: %d\n", p.ComputeNonAllergenCount())
 	ingredients := p.ComputeDangerousIngredientsList()
+	l := formatList(ingredients)
+	fmt.Printf("Allergen list: %s\n", l)
+}
+
+func formatList(ingredients []allergen.Ingredient) string {
 	b := strings.Builder{}
 	for _, a := range ingredients {
 		b.WriteString(string(a))
 		b.WriteRune(',')
 	}
 	l := b.String()
-	l = l[:len(l)-1]
-	fmt.Printf("Allergen list: %s\n", l)
+	return l[:len(l)-1]
 }
